78703c0b90965aade213f8f6180e84c5db0af503
yuto-yugeta <yugemaru0528@gmail.com>
2023-05-01 15:46:35 +0900

    validatesを一部削除

--- app/models/article.rb
+++ app/models/article.rb
@@ -21,7 +21,5 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :article_likes, dependent: :destroy
   has_many :comments, dependent: :destroy
-  validates :title, presence: true , uniqueness:{ case_sensitive: false}
-  validates :body, presence: true
   validates :user_id, presence: true
 end
