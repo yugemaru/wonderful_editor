80a23449dec4ff4f708db7a787bcb4f5f0fffa9a
yuto-yugeta <yugemaru0528@gmail.com>
2023-05-10 22:16:16 +0900

    rubocopを実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #  index_users_on_uid_and_provider      (uid,provider) UNIQUE
 #
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
