3bbc4bf0585a84f74037aa6e2b601b6c99f055dd
yuto-yugeta <yugemaru0528@gmail.com>
2023-05-11 10:32:24 +0900

    rubocopの指示に従い、編集

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,16 +1,15 @@
-class Api::V1::ArticlesController < ApplicationController
-
-  before_action :set_article , only: %i[ show]
+class Api::V1::ArticlesController < Api::V1::BaseApiController
+  before_action :set_article, only: %i[show]
 
   def index
     articles = Article.order(updated_at: :desc)
-    render json: articles each_serializer: Api::V1::ArticlePreviewSerializer
+    render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
     # binding.pry
   end
 
   def show
     article = Article.find(params[:id])
-    render json: @article,serializer: Api::V1::ArticleSerializer
+    render json: @article, serializer: Api::V1::ArticleSerializer
   end
 
   def create
@@ -18,14 +17,19 @@ class Api::V1::ArticlesController < ApplicationController
     render json: article, serializer: Api::V1::ArticleSerializer
   end
 
+  def update
+    article.update!(article_params)
+    render json: article, serializer: Api::V1::ArticleSerializer
+  end
+
   private
-  def set_article
-    @article = Article.find(params[:id])
-  end
 
+    def set_article
+      @article = Article.find(params[:id])
+    end
 
-  def article_params
-    # binding.pry
-    params.require(:article).permit(:title,:body)
-  end
+    def article_params
+      # binding.pry
+      params.require(:article).permit(:title, :body)
+    end
 end
