34e5c615b6e2a42e4927d0c4535164e85b4f1971
yuto-yugeta <yugemaru0528@gmail.com>
2023-06-04 09:57:03 +0900

    仮置きしていたcurrent_userを削除

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::BaseApiController < ApplicationController
-  def current_user
-    @current_user ||= User.first
-  end
+  # def current_user
+  #   @current_user ||= User.first
+  # end
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :authenticate_api_v1_user!
 end
