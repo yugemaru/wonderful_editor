1930f5d01e7d095e18c106f3ef220efaa48bb613
yuto-yugeta <yugemaru0528@gmail.com>
2023-06-07 16:43:56 +0900

    statusカラムの追加に際してserialiserを編集

--- app/serializers/api/v1/article_preview_serializer.rb
+++ app/serializers/api/v1/article_preview_serializer.rb
@@ -1,4 +1,4 @@
 class Api::V1::ArticlePreviewSerializer < ActiveModel::Serializer
-  attributes :id, :title, :updated_at
+  attributes :id, :title, :updated_at, :status
   belongs_to :user, serializer: Api::V1::UserSerializer
 end
--- app/serializers/api/v1/article_serializer.rb
+++ app/serializers/api/v1/article_serializer.rb
@@ -1,4 +1,4 @@
 class Api::V1::ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :updated_at ,:status
+  attributes :id, :title, :body, :updated_at, :status
   belongs_to :user, serializer: Api::V1::UserSerializer
 end
